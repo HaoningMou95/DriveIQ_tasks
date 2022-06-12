b654b6d6aa252e8accaebcdf8458c1e467cd76b7
Haoning Mou <muhaoning@hotmail.com>
2022-06-12 21:36:37 +1000

    add loading button

--- task1/src/components/DynamicTable.js
+++ task1/src/components/DynamicTable.js
@@ -1,49 +1,67 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import axios from 'axios'
+import { CircularProgress } from '@mui/material'
 import { useEffect, useState } from 'react'
+import { fetchData } from '../util/Fetch'
 
 
 function DynamicTable () {
     const [data, setData] = useState([])
-    useEffect(()=>{
+    const [loading,setLoading] = useState(false)
+
+    const handleOnLoading = () => {
+        setLoading(true)
         axios.get(`http://universities.hipolabs.com/search?country=Australia`)
-                .then((res)=>{
-                    setData(res.data)
-                    console.log('Result',data)
-                })
-                .catch((error)=>{
-                    console.log(error)
-                })
-    },[])
+                    .then((res)=>{
+                        setData(res.data)
+                        setLoading(false)
+                        console.log('!!!!!!!',res)
+                    })
+    }
 
     return(
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>domains</TableCell>
-                        <TableCell>web_pages</TableCell>
-                        <TableCell>state_province</TableCell>
-                        <TableCell>name</TableCell>
-                        <TableCell>country</TableCell>
-                        <TableCell>alpha_two_code</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((row) => (
-                        <TableRow key={row.domains}>
-                            <TableCell>{row.domains}</TableCell>
-                            <TableCell>{row.web_pages}</TableCell>
-                            <TableCell>{!row['state-province']&&'NULL'}  </TableCell>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.country}</TableCell>
-                            <TableCell>{row.alpha_two_code}</TableCell>
-
+        <div>
+            {loading && 
+                <CircularProgress sx={{
+                    position: 'absolute',
+                    marginTop:30,
+                    zIndex: 1,}}
+                />}
+            <div>
+            <Button onClick={()=>handleOnLoading()}>Loading</Button>
+            <Button>DELETE</Button>
+            <Button>ADD</Button>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>domains</TableCell>
+                            <TableCell>web_pages</TableCell>
+                            <TableCell>state_province</TableCell>
+                            <TableCell>name</TableCell>
+                            <TableCell>country</TableCell>
+                            <TableCell>alpha_two_code</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((row) => (
+                            <TableRow key={row.domains}>
+                                <TableCell>{row.domains}</TableCell>
+                                <TableCell>{row.web_pages}</TableCell>
+                                <TableCell>{!row['state-province']&&'NULL'}  </TableCell>
+                                <TableCell>{row.name}</TableCell>
+                                <TableCell>{row.country}</TableCell>
+                                <TableCell>{row.alpha_two_code}</TableCell>
+
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </div>
+            
+        </div>
+        
     )
 }
 
