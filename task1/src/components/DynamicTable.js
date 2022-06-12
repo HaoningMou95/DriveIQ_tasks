662ebe443679a4c87b5b0431dee6fc1d9481293f
Haoning Mou <muhaoning@hotmail.com>
2022-06-12 22:34:36 +1000

    implement loading, add, delete button

--- task1/src/components/DynamicTable.js
+++ task1/src/components/DynamicTable.js
@@ -1,22 +1,35 @@
 import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import axios from 'axios'
 import { CircularProgress } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { fetchData } from '../util/Fetch'
+import { useState } from 'react'
 
 
 function DynamicTable () {
     const [data, setData] = useState([])
-    const [loading,setLoading] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-    const handleOnLoading = () => {
+    const onLoading = async () => {
         setLoading(true)
         axios.get(`http://universities.hipolabs.com/search?country=Australia`)
-                    .then((res)=>{
-                        setData(res.data)
-                        setLoading(false)
-                        console.log('!!!!!!!',res)
-                    })
+            .then((res)=>{
+                setData(res.data)
+                setLoading(false)
+            })
+            .catch((error=>{
+                console.log(error)
+                alert('Fail to fetch data.')
+            }))         
+    }
+
+    const handleDelete = () => {
+        const newData = data.slice(0, data.length-1)
+        setData(newData)
+    }
+
+    const handleAdd = () => {
+        const firstEle = [data[0]]
+        const newData = data.concat(firstEle)
+        setData(newData)
     }
 
     return(
@@ -24,44 +37,41 @@ function DynamicTable () {
             {loading && 
                 <CircularProgress sx={{
                     position: 'absolute',
-                    marginTop:30,
+                    marginTop: 30,
                     zIndex: 1,}}
                 />}
             <div>
-            <Button onClick={()=>handleOnLoading()}>Loading</Button>
-            <Button>DELETE</Button>
-            <Button>ADD</Button>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>domains</TableCell>
-                            <TableCell>web_pages</TableCell>
-                            <TableCell>state_province</TableCell>
-                            <TableCell>name</TableCell>
-                            <TableCell>country</TableCell>
-                            <TableCell>alpha_two_code</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map((row) => (
-                            <TableRow key={row.domains}>
-                                <TableCell>{row.domains}</TableCell>
-                                <TableCell>{row.web_pages}</TableCell>
-                                <TableCell>{!row['state-province']&&'NULL'}  </TableCell>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.country}</TableCell>
-                                <TableCell>{row.alpha_two_code}</TableCell>
-
+                <Button onClick={onLoading}>Loading</Button>
+                <Button onClick={handleDelete}>DELETE</Button>
+                <Button  onClick={handleAdd}>ADD</Button>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>domains</TableCell>
+                                <TableCell>web_pages</TableCell>
+                                <TableCell>state-province</TableCell>
+                                <TableCell>name</TableCell>
+                                <TableCell>country</TableCell>
+                                <TableCell>alpha_two_code</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {data.map((row, i) => (
+                                <TableRow key={i}>
+                                    <TableCell>{row.domains}</TableCell>
+                                    <TableCell>{row.web_pages}</TableCell>
+                                    <TableCell>{!row['state-province']&&'NULL'}</TableCell>
+                                    <TableCell>{row.name}</TableCell>
+                                    <TableCell>{row.country}</TableCell>
+                                    <TableCell>{row.alpha_two_code}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
-            
         </div>
-        
     )
 }
 
