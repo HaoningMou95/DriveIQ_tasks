8e80250b35ecd12e413a18320334b7c5f3f13aaa
Haoning Mou <muhaoning@hotmail.com>
2022-06-14 22:04:30 +1000

    set up entriesrouter

--- task2/app.js
+++ task2/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var entriesRouter = require('./routes/entries');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/entries', entriesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +36,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const axios = require('axios');
+
+axios.get('http://webcode.me').then(resp => {
+
+    console.log(resp.data);
 });
 
 module.exports = app;
---
+++ task2/routes/entries.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+const {dataFilter} = require('../util/dataUtils')
+
+router.get('/', function(req, res) {
+  const category = req.query.category || null
+  const limit = req.query.limit || null
+  let response
+  axios.get('https://api.publicapis.org/entries').then((result) => {
+    response = dataFilter(result.data, category, limit)
+    res.send(response)
+  }).catch((err) => {
+    console.log(err)
+  })
+});
+
+module.exports = router;
