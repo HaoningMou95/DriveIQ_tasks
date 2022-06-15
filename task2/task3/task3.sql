378c04efd9b9c7afb4519959faf9da8976b112c7
Haoning Mou <48826920+LoganMou@users.noreply.github.com>
2022-06-15 23:27:38 +1000

    Update task3.sql

--- task2/task3/task3.sql
+++ task2/task3/task3.sql
@@ -1,5 +1,5 @@
 SELECT b.name as 'Broker_Name', count(b.name) as 'Customer_Count'
-FROM lp_wallet_card.CUSTOMER as c , lp_wallet_card.BROKER as b
+FROM CUSTOMER as c , BROKER as b
 where c.BROKER_ID = b.ID
 group by Broker_Name
 order by Customer_Count desc, Broker_Name asc;
